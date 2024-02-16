9716266a0c861219de0c7befa8d07ac832c58642
Bewin-btw <aktaeverikwork@gmail.com>
2024-02-16 19:26:26 +0600

    classes of db added

---
+++ src/main/java/com/Hotel/hotelreservationsystem/model/Payment.java
@@ -0,0 +1,13 @@
+package com.Hotel.hotelreservationsystem.model;
+
+import java.time.LocalDate;
+
+public class Payment {
+    private int paymentID;
+    private int reservationID;
+    private double amount;
+    private LocalDate paymentDate;
+    private String paymentMethod;
+
+    // Constructors, getters, and setters
+}
